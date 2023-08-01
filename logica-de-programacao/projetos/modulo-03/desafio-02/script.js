ed7234614edc4a5f38e96193d682b61f329b3c4a
Isaac Almeida <isaacalmeidafilho@gmail.com>
2023-08-01 18:19:20 -0300

    desafio-02

---
+++ logica-de-programacao/projetos/modulo-03/desafio-02/script.js
@@ -0,0 +1,12 @@
+// Nesta atividade, você deve criar um código em JavaScript com uma matriz bidimensional para armazenar informações sobre animais e as suas características.
+
+// Cada linha deve representar um animal e cada coluna terá as diferentes informações sobre ele, como nome, espécie e idade.
+
+const animais = [
+  ['Cachorro', 'Mamífero', 10],
+  ['Elefante', 'Mamífero', 7],
+  ['Cobra', 'Réptil', 4],
+  ['Águia', 'Ave', 6],
+]
+
+console.log(animais[0][0]);
